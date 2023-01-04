5bfb88e26a0eb0f2db39cdbb4fa096acd73e5cd9
sawa <sawami0512@gmail.com>
2023-01-05 07:22:20 +0900

    Little Modified

--- App.jsx
+++ App.jsx
@@ -1,15 +1,6 @@
+/* eslint-disable react/react-in-jsx-scope */
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      {/* eslint-disable*/}
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -19,3 +10,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default function App() {
+  return (
+    // eslint-disable-next-line react/react-in-jsx-scope
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      {/* eslint-disable*/}
+      <StatusBar style="auto" />
+    </View>
+  );
+}
