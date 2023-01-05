727e37fd619fa5d0b9ebfc718590e5a7af4086da
sawa <sawami0512@gmail.com>
2023-01-05 18:40:12 +0900

    Style MemoList Screen

--- App.jsx
+++ App.jsx
@@ -9,7 +9,7 @@ const styles = StyleSheet.create({
   appbar: {
     width: '100%',
     height: 104,
-    backgroudColor: '#467FD3',
+    backgroundColor: '#467FD3',
     justifyContent: 'flex-end',
   },
   appbarInner: {
@@ -18,7 +18,8 @@ const styles = StyleSheet.create({
   appbarRight: {
     position: 'absolute',
     right: 19,
-    bottom: 8,
+    bottom: 16,
+    color: 'rgba(255,255,255,0.8)',
   },
   appbarTitle: {
     marginBottom: 8,
@@ -26,6 +27,48 @@ const styles = StyleSheet.create({
     lineHeight: 32,
     color: '#ffffff',
     fontWeight: 'bold',
+  },
+  memoListItem: {
+    backgroundColor: '#ffffff',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 16,
+    paddingHorizontal: 19,
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: 'rgba(0,0,0,0,15)',
+  },
+  memoListItemTitle: {
+    fontsize: 16,
+    lightHeight: 12,
+
+  },
+  memoListItemDate: {
+    fontsize: 12,
+    lightHeight: 16,
+    color: '#848484',
+
+  },
+  circleButton: {
+    backgroundColor: '#467FD3',
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    right: 40,
+    bottom: 40,
+    shadowColor: '#000000',
+    shadowOffset: { width: 0, height: 8 },
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    elevation: 8,
+  },
+  circleButtonLabel: {
+    color: '#ffffff',
+    fontSize: 40,
+    lineHeight: 40,
   },
 });
 
@@ -35,47 +78,50 @@ export default function App() {
       {/* ログアウト */}
       <View style={styles.appbar}>
         <View style={styles.appbarInner}>
-          <Text styke={styles.appbarTitle}>Memo App</Text>
+          <Text style={styles.appbarTitle}>Memo App</Text>
           <Text style={styles.appbarRight}>ログアウト</Text>
         </View>
       </View>
       {/* 買い物リスト */}
       <View>
-        <View>
+        <View style={styles.memoListItem}>
           <View>
-            <Text>買い物リスト</Text>
-            <Text>2020年12月24日</Text>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2020年12月24日</Text>
           </View>
           <View>
             <Text>X</Text>
           </View>
         </View>
       </View>
+      {/* 買い物リスト */}
       <View>
-        <View>
+        <View style={styles.memoListItem}>
           <View>
-            <Text>買い物リスト</Text>
-            <Text>2020年12月24日</Text>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2020年12月24日</Text>
           </View>
           <View>
             <Text>X</Text>
           </View>
         </View>
       </View>
+      {/* 買い物リスト */}
       <View>
-        <View>
+        <View style={styles.memoListItem}>
           <View>
-            <Text>買い物リスト</Text>
-            <Text>2020年12月24日</Text>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2020年12月24日</Text>
           </View>
           <View>
             <Text>X</Text>
           </View>
         </View>
       </View>
+
       {/* プラスボタン */}
-      <View>
-        <Text>+</Text>
+      <View style={styles.circleButton}>
+        <Text style={styles.circleButtonLabel}>+</Text>
       </View>
 
     </View>
