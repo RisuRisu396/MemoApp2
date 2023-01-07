fd6c14a9ec4308dc8bdb0c5a30771c739560fa79
sawa <sawami0512@gmail.com>
2023-01-07 14:51:14 +0900

    Create MemoDetailScreen

--- src/screens/MemoDetailScreen.jsx
+++ src/screens/MemoDetailScreen.jsx
@@ -9,17 +9,17 @@ export default function MemoDetailScreen() {
   return (
     <View style={styles.container}>
       <AppBar />
-      <View>
-        <Text>買い物リスト</Text>
-        <Text>2020年12月24日</Text>
+      <View style={styles.memoHeader}>
+        <Text style={styles.memoTitle}>買い物リスト</Text>
+        <Text style={styles.memoDate}> 2020年12月24日</Text>
       </View>
-      <ScrollView>
-        <Text>
+      <ScrollView style={styles.memoBody}>
+        <Text style={styles.memoText}>
           買い物リスト
           書体やレイアウトなどを確認するために用います
         </Text>
       </ScrollView>
-      <CircleButton>+</CircleButton>
+      <CircleButton style={{ top: 160, buttom: 'auto' }}>+</CircleButton>
     </View>
   );
 }
@@ -28,5 +28,31 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#ffffff',
+  },
+  memoHeader: {
+    backgroundColor: '#467FD3',
+    height: 96,
+    justifyContent: 'center',
+    paddingVertical: 24,
+    paddingHorizontal: 19,
+  },
+  memoTitle: {
+    color: '#ffffff',
+    fontSize: 20,
+    lineHeight: 32,
+    fontWeight: 'bold',
+  },
+  memoDate: {
+    color: '#ffffff',
+    fontSize: 12,
+    lineHeight: 16,
+  },
+  memoBody: {
+    paddingVertical: 32,
+    paddingHorizontal: 27,
+  },
+  memoText: {
+    fontSize: 16,
+    lineHeight: 24,
   },
 });
