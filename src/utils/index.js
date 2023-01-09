ea3ffe3402af7ed5cf6033a6f790adab083659d1
sawa <sawami0512@gmail.com>
2023-01-09 15:08:25 +0900

    memo detail screen

---
+++ src/utils/index.js
@@ -0,0 +1,7 @@
+// 日付を文字列に変換
+import { format } from 'date-fns';
+// eslint-disable-next-line import/prefer-default-export
+export function dateToString(date) {
+  if (!date) { return ''; }
+  return format(date, 'yyyy年M月d日 HH時mm分');
+}
