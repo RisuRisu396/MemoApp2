ca634a9dd6484766e8f07f3e18414e610c831843
sawa <sawami0512@gmail.com>
2023-01-09 14:30:26 +0900

    Fetch data from firestore

--- src/components/MemoList.jsx
+++ src/components/MemoList.jsx
@@ -1,14 +1,18 @@
 import { Feather } from '@expo/vector-icons';
 import React from 'react';
 import {
-  View, Text, StyleSheet, TouchableOpacity, Alert,
+  View, Text, StyleSheet, TouchableOpacity, Alert, FlatList,
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import {
+  shape, string, instanceOf, arrayOf,
+} from 'prop-types';
 
-export default function MemoList() {
+export default function MemoList(props) {
+  const { memos } = props;
   const navigation = useNavigation();
-  return (
-    <View>
+  function renderItem({ item }) {
+    return (
       <TouchableOpacity
         style={styles.memoListItem}
         onPress={() => {
@@ -16,8 +20,8 @@ export default function MemoList() {
         }}
       >
         <View>
-          <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-          <Text style={styles.memoListItemDate}>2020年12月24日</Text>
+          <Text style={styles.memoListItemTitle} numberOfLines={1}>{item.bodyText}</Text>
+          <Text style={styles.memoListItemDate}>{String(item.updatedAt)}</Text>
         </View>
         <TouchableOpacity
           style={styles.memoDelete}
@@ -26,10 +30,36 @@ export default function MemoList() {
           <Feather name="x" size={16} color="#B0B0B0" />
         </TouchableOpacity>
       </TouchableOpacity>
+
+    );
+  }
+
+  return (
+    // memoを受け取る
+    <View style={styles.container}>
+      <FlatList
+        // memoを効率的にレンダリングする
+        data={memos}
+        // eslint-disable-next-line react/jsx-no-bind
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
     </View>
   );
 }
+
+MemoList.propTypes = {
+  memos: arrayOf(shape({
+    id: string,
+    bodyText: string,
+    updatedAt: instanceOf(Date),
+  })).isRequired,
+};
+
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   memoListItem: {
     backgroundColor: '#ffffff',
     flexDirection: 'row',
